04067456b8d14867a25fabab3d9d50a92a91f435
Bartosz Bieniek <itsbk20@gmail.com>
2022-10-24 09:53:51 +0200

    Generate player-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlayerListComponent } from './player-list/player-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '/players', component: PlayerListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
