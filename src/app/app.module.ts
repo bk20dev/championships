071e6469bd88157a273d3c63be8f6e9d69bc7243
Bartosz Bieniek <itsbk20@gmail.com>
2022-10-24 10:04:12 +0200

    Create app-navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerListComponent } from './player-list/player-list.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerListComponent
+    PlayerListComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
