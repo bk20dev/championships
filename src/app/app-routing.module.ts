adb64e86ea89b15323115726eb5628e24860e250
Bartosz Bieniek <itsbk20@gmail.com>
2022-10-24 10:01:57 +0200

    Configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayerListComponent } from './player-list/player-list.component';
 
-const routes: Routes = [{ path: '/players', component: PlayerListComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/players', pathMatch: 'full' },
+  { path: 'players', component: PlayerListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
