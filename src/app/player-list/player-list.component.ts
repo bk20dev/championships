a9e9b91af459c45c8a92e88e8425644b93853ebc
Bartosz Bieniek <itsbk20@gmail.com>
2022-11-14 14:18:48 +0100

    Create player list component

--- src/app/player-list/player-list.component.ts
+++ src/app/player-list/player-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from 'src/domain/Player';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
-  styleUrls: ['./player-list.component.scss']
+  styleUrls: ['./player-list.component.scss'],
 })
 export class PlayerListComponent implements OnInit {
+  players: Player[] = [];
 
-  constructor() { }
+  constructor(private playerService: PlayerService) {}
 
   ngOnInit(): void {
+    this.playerService
+      .getAllPlayers()
+      .subscribe((players) => (this.players = players));
   }
-
 }
