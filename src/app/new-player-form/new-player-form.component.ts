2a9a2795eaa4d1ccd0ec6c9fe8999305df429782
Bartosz Bieniek <itsbk20@gmail.com>
2022-11-25 19:38:38 +0100

    Update new-player-form to use player-form component

--- src/app/new-player-form/new-player-form.component.ts
+++ src/app/new-player-form/new-player-form.component.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { oneOf } from "../../api/validator";
 import { PlayerService } from "../player.service";
-import { Player } from "../../domain/Player";
 import { Router } from "@angular/router";
+import { PlayerChangeEvent } from "../player-form/player-form.component";
+import { Player } from "../../domain/Player";
 
 @Component({
   selector: "app-new-player-form",
@@ -11,34 +10,22 @@ import { Router } from "@angular/router";
   styleUrls: ["./new-player-form.component.scss"],
 })
 export class NewPlayerFormComponent {
-  readonly playerPositions = ["keeper", "half-back", "sweeper", "forward"];
-
-  playerForm = new FormGroup({
-    firstName: new FormControl("", [
-      Validators.required, Validators.minLength(2),
-    ]),
-    lastName: new FormControl("", [
-      Validators.required, Validators.minLength(2),
-    ]),
-    dateOfBirth: new FormControl("", Validators.required),
-    club: new FormControl("", Validators.required),
-    position: new FormControl(this.playerPositions[0], [
-      Validators.required, oneOf(this.playerPositions),
-    ]),
-  });
+  isValid = false;
+  player!: Omit<Player, "id">;
 
   constructor(private playerService: PlayerService, private router: Router) {
   }
 
-  onSubmit(): void {
-    if (this.playerForm.valid) {
-      const player = this.playerForm.value as Omit<Player, "id">;
-      this.playerService
-        .createPlayer(player)
-        .subscribe((player) => {
-          // noinspection JSIgnoredPromiseFromCall
-          this.router.navigate(["players", player.id]);
-        });
-    }
+  onPlayerChange(event: PlayerChangeEvent): void {
+    this.isValid = event.valid;
+    this.player = event.player;
+  }
+
+  createPlayer() {
+    this.playerService
+      .createPlayer(this.player)
+      .subscribe(async (player) => {
+        await this.router.navigate(["players", player.id]);
+      });
   }
 }
