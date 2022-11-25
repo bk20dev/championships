00e77efb329df57d267b3153fbbac7b5ddc8e3c1
Bartosz Bieniek <itsbk20@gmail.com>
2022-11-25 17:17:29 +0100

    Define form in player-form component

--- src/app/player-form/player-form.component.ts
+++ src/app/player-form/player-form.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { oneOf } from "../../api/validator";
+import { Player } from "../../domain/Player";
+
+export interface PlayerChangeEvent {
+  valid: boolean,
+  player: Omit<Player, "id">
+}
 
 @Component({
-  selector: 'app-player-form',
-  templateUrl: './player-form.component.html',
-  styleUrls: ['./player-form.component.scss']
+  selector: "app-player-form",
+  templateUrl: "./player-form.component.html",
+  styleUrls: ["./player-form.component.scss"],
 })
 export class PlayerFormComponent implements OnInit {
+  @Output()
+  readonly playerChange = new EventEmitter<PlayerChangeEvent>();
 
-  constructor() { }
+  readonly playerPositions = ["keeper", "half-back", "sweeper", "forward"];
+
+  readonly playerForm = new FormGroup({
+    firstName: new FormControl("", [
+      Validators.required, Validators.minLength(2),
+    ]),
+    lastName: new FormControl("", [
+      Validators.required, Validators.minLength(2),
+    ]),
+    dateOfBirth: new FormControl("", Validators.required),
+    club: new FormControl("", Validators.required),
+    position: new FormControl(this.playerPositions[0], [
+      Validators.required, oneOf(this.playerPositions),
+    ]),
+  });
+
+  constructor() {
+  }
+
+  @Input()
+  set player(player: Omit<Player, "id">) {
+    const { firstName, lastName, dateOfBirth, club, position } = player;
+    const playerValue = { firstName, lastName, dateOfBirth, club, position };
+    this.playerForm.setValue(playerValue);
+    this.onChange();
+  }
 
   ngOnInit(): void {
   }
 
+  onChange(): void {
+    const { valid, value } = this.playerForm;
+    const event: PlayerChangeEvent = {
+      valid, player: value as Omit<Player, "id">,
+    };
+    this.playerChange.emit(event);
+  }
 }
