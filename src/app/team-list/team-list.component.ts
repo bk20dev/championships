589d2d032bc373cc67702c08d205fcfad60c99a1
Bartosz Bieniek <itsbk20@gmail.com>
2022-11-27 17:23:21 +0100

    Implement team-list component

--- src/app/team-list/team-list.component.ts
+++ src/app/team-list/team-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Team } from "../../domain/Team";
+import { TeamService } from "../team.service";
 
 @Component({
-  selector: 'app-team-list',
-  templateUrl: './team-list.component.html',
-  styleUrls: ['./team-list.component.scss']
+  selector: "app-team-list",
+  templateUrl: "./team-list.component.html",
+  styleUrls: ["./team-list.component.scss"],
 })
 export class TeamListComponent implements OnInit {
+  teams: Team[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private teamService: TeamService) {
   }
 
+  ngOnInit(): void {
+    this.teamService
+      .getAllTeams()
+      .subscribe((teams) => (this.teams = teams));
+  }
 }
