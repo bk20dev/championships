74169f1357de590f8a9e7b894cb2700d2cd7b683
Bartosz Bieniek <itsbk20@gmail.com>
2022-11-27 21:46:28 +0100

    Implement team-form component

--- src/app/team-form/team-form.component.ts
+++ src/app/team-form/team-form.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Team } from "../../domain/Team";
+
+export interface TeamChangeEvent {
+  valid: boolean,
+  team: Omit<Team, "id">
+}
 
 @Component({
-  selector: 'app-team-form',
-  templateUrl: './team-form.component.html',
-  styleUrls: ['./team-form.component.scss']
+  selector: "app-team-form",
+  templateUrl: "./team-form.component.html",
+  styleUrls: ["./team-form.component.scss"],
 })
-export class TeamFormComponent implements OnInit {
+export class TeamFormComponent {
+  readonly teamForm = new FormGroup({
+    name: new FormControl("", [
+      Validators.required, Validators.minLength(2),
+    ]),
+  });
 
-  constructor() { }
+  @Output()
+  readonly teamChange = new EventEmitter<TeamChangeEvent>();
 
-  ngOnInit(): void {
+  @Input()
+  set team(team: Omit<Team, "id">) {
+    const { name } = team;
+    this.teamForm.setValue({ name });
+    this.onChange();
   }
 
+  onChange(): void {
+    const { valid, value } = this.teamForm;
+    const event: TeamChangeEvent = {
+      valid, team: value as Omit<Team, "id">,
+    };
+    this.teamChange.emit(event);
+  }
 }
