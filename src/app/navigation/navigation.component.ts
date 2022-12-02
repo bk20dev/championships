d1d81a394f7c914fee90fd75b0612d05d357fafa
Bartosz Bieniek <itsbk20@gmail.com>
2022-12-02 23:28:33 +0100

    Add icons to the sidebar

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -13,7 +13,7 @@ interface Link {
 })
 export class NavigationComponent {
   readonly links: Link[] = [
-    { label: "Players", link: "/players", iconPath: "" },
-    { label: "Teams", link: "/teams", iconPath: "" },
+    { label: "Players", link: "/players", iconPath: "assets/person_FILL0_wght400_GRAD0_opsz24.svg" },
+    { label: "Teams", link: "/teams", iconPath: "assets/workspaces_FILL0_wght400_GRAD0_opsz24.svg" },
   ];
 }
