c5c8d5e5a35c615a694d18f7d689c2d375b044f6
Bartosz Bieniek <itsbk20@gmail.com>
2022-12-06 22:12:26 +0100

    Generate player-picker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,29 @@
-import { HttpClientModule } from "@angular/common/http";
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NavigationComponent } from "./navigation/navigation.component";
-import { PlayerListComponent } from "./players/player-list/player-list.component";
-import { AgePipe } from "./age.pipe";
-import { PlayerDetailsComponent } from "./players/player-details/player-details.component";
-import { NewPlayerFormComponent } from "./players/new-player-form/new-player-form.component";
-import { ReactiveFormsModule } from "@angular/forms";
-import { EditPlayerFormComponent } from "./players/edit-player-form/edit-player-form.component";
-import { PlayerFormComponent } from "./players/player-form/player-form.component";
-import { TeamListComponent } from "./teams/team-list/team-list.component";
-import { TeamDetailsComponent } from "./teams/team-details/team-details.component";
-import { TeamFormComponent } from "./teams/team-form/team-form.component";
-import { NewTeamFormComponent } from "./teams/new-team-form/new-team-form.component";
-import { EditTeamFormComponent } from './teams/edit-team-form/edit-team-form.component';
+import {HttpClientModule} from "@angular/common/http";
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {NavigationComponent} from "./navigation/navigation.component";
+import {PlayerListComponent} from "./players/player-list/player-list.component";
+import {AgePipe} from "./age.pipe";
+import {PlayerDetailsComponent} from "./players/player-details/player-details.component";
+import {NewPlayerFormComponent} from "./players/new-player-form/new-player-form.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {EditPlayerFormComponent} from "./players/edit-player-form/edit-player-form.component";
+import {PlayerFormComponent} from "./players/player-form/player-form.component";
+import {TeamListComponent} from "./teams/team-list/team-list.component";
+import {TeamDetailsComponent} from "./teams/team-details/team-details.component";
+import {TeamFormComponent} from "./teams/team-form/team-form.component";
+import {NewTeamFormComponent} from "./teams/new-team-form/new-team-form.component";
+import {EditTeamFormComponent} from './teams/edit-team-form/edit-team-form.component';
+import {PlayerPickerComponent} from './teams/player-picker/player-picker.component';
 
 @NgModule({
   declarations: [
     AgePipe, AppComponent, PlayerListComponent, NavigationComponent,
     PlayerDetailsComponent, NewPlayerFormComponent, EditPlayerFormComponent,
     PlayerFormComponent, TeamListComponent, TeamDetailsComponent,
-    TeamFormComponent, NewTeamFormComponent, EditTeamFormComponent,
+    TeamFormComponent, NewTeamFormComponent, EditTeamFormComponent, PlayerPickerComponent,
   ],
   imports: [
     BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule,
---
+++ src/app/teams/player-picker/player-picker.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-player-picker',
+  templateUrl: './player-picker.component.html',
+  styleUrls: ['./player-picker.component.scss']
+})
+export class PlayerPickerComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
