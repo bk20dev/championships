89c317a8ee2ef36d39fba61b6e2399c2498d977d
Bartosz Bieniek <itsbk20@gmail.com>
2022-12-07 23:14:11 +0100

    Create basic styles for player-search component

--- src/app/teams/player-search/player-search.component.ts
+++ src/app/teams/player-search/player-search.component.ts
@@ -1,16 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Player} from "../../../domain/Player";
 
 @Component({
   selector: 'app-player-search',
   templateUrl: './player-search.component.html',
   styleUrls: ['./player-search.component.scss']
 })
-export class PlayerSearchComponent implements OnInit {
+export class PlayerSearchComponent {
+
+  @Input()
+  players: Player[] = [];
+
+  @Output()
+  readonly select = new EventEmitter<Player>();
 
   constructor() {
   }
-
-  ngOnInit(): void {
-  }
-
 }
